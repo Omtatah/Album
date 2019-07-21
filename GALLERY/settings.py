58d368c553055bb03f8dc2de0c192baa3806f63a
Omtatah <murtallahomtatah@gmail.com>
2019-07-21 20:11:34 +0300

    change the autogenerated SECRET_KEY to something easy and memorable to ease deployment

--- GALLERY/settings.py
+++ GALLERY/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h2zakxa^qz%0t_ejxl)2+!7c=ezn+==w@2bta41pl17lk&8o(w'
+SECRET_KEY = '12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
