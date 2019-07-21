6152b7b2a4cff5362c5c4190074a64804af0ede4
Omtatah <murtallahomtatah@gmail.com>
2019-07-21 19:38:06 +0300

    make and activate the virtual enironment

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/omtatah/Documents/moringa-school-projects/Album/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
